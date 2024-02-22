917f3091277026694a7e4b02899799de0c9c7b6f
Zumayaa <abzu_21@alu.uabcs.mx>
2024-02-21 18:43:27 -0700

    lesgo

--- src/App.js
+++ src/App.js
@@ -6,10 +6,12 @@ function App() {
     <div className="App">
       <header className='header'>
         <nav className='nav'>
+          <img src="https://png.pngtree.com/png-clipart/20220923/original/pngtree-chef-and-spatula-logo-png-image_8628649.png" alt="" />
           <h1 className='title-page'>
             Recetas de comida
           </h1>
           <input type="text" />
+          <button>Buscar</button>
         </nav>
       </header>
       <div className='father'>
@@ -25,6 +27,7 @@ function App() {
           </div>
         </div>
         <div className='product'>
+          <img src="https://i5.walmartimages.com.mx/gr/images/product-images/img_large/00750100010306L.jpg" alt="" />
           <div className='content'>
             <h1 className='product-title'>
               Producto 2
@@ -34,6 +37,7 @@ function App() {
               atque harum optio sed, nulla cumque repellendus aperiam saepe quibusdam rem,
             </p>
           </div>
+          <button>Ver</button>
         </div>
         <div className='product'>
           <div className='content'>
