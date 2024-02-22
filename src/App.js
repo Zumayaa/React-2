0ed19f388b916dbe7b63636ed7b888a11fd69a72
Zumayaa <abzu_21@alu.uabcs.mx>
2024-02-21 18:13:34 -0700

    push just in case xd

--- src/App.js
+++ src/App.js
@@ -4,20 +4,49 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className='header'>
+        <nav className='nav'>
+          <h1 className='title-page'>
+            Recetas de comida
+          </h1>
+          <input type="text" />
+        </nav>
       </header>
+      <div className='father'>
+        <div className='product'>
+          <div className='content'>
+            <h1 className='product-title'>
+              Producto 1
+            </h1>
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eos
+              atque harum optio sed, nulla cumque repellendus aperiam saepe quibusdam rem,
+            </p>
+          </div>
+        </div>
+        <div className='product'>
+          <div className='content'>
+            <h1 className='product-title'>
+              Producto 2
+            </h1>
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eos
+              atque harum optio sed, nulla cumque repellendus aperiam saepe quibusdam rem,
+            </p>
+          </div>
+        </div>
+        <div className='product'>
+          <div className='content'>
+            <h1 className='product-title'>
+              Producto 3
+            </h1>
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eos
+              atque harum optio sed, nulla cumque repellendus aperiam saepe quibusdam rem,
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
